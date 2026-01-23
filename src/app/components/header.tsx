de84de21e6bc7678ebc6d08319611cefd37208ea
Babatunde Ojo <babatundeo@theluxev.com>
2026-01-23 14:27:17 +0100

    logo

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -39,7 +39,7 @@ export function Header() {
       <div className="container mx-auto px-6 flex items-center justify-between">
         {/* Logo */}
         <Link to="/" className="flex items-center gap-3 group">
-          <img src={Logo} alt="PK5 Mining Logo" className="h-16 w-40" />
+          <img src={Logo} alt="PK5 Mining Logo" className="w-40 h-auto object-contain" />
         </Link>
 
         {/* Desktop Navigation */}
