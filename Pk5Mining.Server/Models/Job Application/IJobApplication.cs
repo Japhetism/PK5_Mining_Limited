69e7b822e6d4da6764bf92a45129dd84be75854a
Abdullahi Oladimeji <AbdullahiO@theluxev.com>
2026-02-11 12:24:42 +0100

    Job Application

--- Pk5Mining.Server/Models/Job Application/IJobApplication.cs
+++ Pk5Mining.Server/Models/Job Application/IJobApplication.cs
@@ -11,7 +11,7 @@ namespace Pk5Mining.Server.Models.Job_Application
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
         public string? Email { get; set; }
-        public string? Phone { get; set; }
+        public string? PhoneNumber { get; set; }
         public string? Country { get; set; }
         public string? Resume { get; set; }
         public string? Status { get; set; }
---
+++ Pk5Mining.Server/Models/Response/ResponseModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Pk5Mining.Server.Models.Response
+{
+    public class ResponseModel
+    {
+        public string? ResponseMessage { get; set; }
+        public object? ResponseData { get; set; }
+        public string? ResponseStatus { get; set; }
+    }
+}
