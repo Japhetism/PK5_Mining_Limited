e2c97c7bb1a0be14ac1e26dce7a3192b684524b0
Babatunde Ojo <babatundeo@theluxev.com>
2026-02-26 12:31:33 +0100

    add flag to enable and disable admin

--- Pk5Mining.Website/src/app/routes/index.tsx
+++ Pk5Mining.Website/src/app/routes/index.tsx
@@ -1,7 +1,16 @@
-import { useRoutes } from "react-router-dom";
+import { useRoutes, Navigate } from "react-router-dom";
 import { clientRoutes } from "./client-routes";
 import { adminRoutes } from "./admin-routes";
 
+const allowAdminFeatures = import.meta.env.VITE_ALLOW_ADMIN_FEATURES === "true";
+
 export function AppRoutes() {
-  return useRoutes([...clientRoutes, ...adminRoutes]);
+  const routes = [
+    ...clientRoutes,
+    ...(allowAdminFeatures ? adminRoutes : []),
+    
+    { path: "*", element: <Navigate to="/" replace /> } 
+  ];
+
+  return useRoutes(routes);
 }
