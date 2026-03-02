fcc68600f0a867fbbafa2dc4d4b0be28502100b3
Abdullahi Oladimeji <AbdullahiO@theluxev.com>
2026-03-02 14:24:22 +0100

    Bug Fix

--- Pk5Mining.Server/Program.cs
+++ Pk5Mining.Server/Program.cs
@@ -132,6 +132,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors("MyAllowSpecificOrigins");
 app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
