504eab155d71b874c790714993b49a37930f6a64
Babatunde Ojo <babatundeo@theluxev.com>
2026-03-12 21:01:44 +0100

    change app loader

--- Pk5Mining.Website/src/app/auth/RequireAdmin.tsx
+++ Pk5Mining.Website/src/app/auth/RequireAdmin.tsx
@@ -1,10 +1,21 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
+import Logo from "../../assets/images/logo.png";
 
 export function RequireAdmin() {
   const { isAuthenticated, isAdmin, isLoading } = useAuth();
 
-  if (isLoading) return <div>Loading...</div>;
+  if (isLoading)
+    return (
+      <div>
+        <img
+          src={Logo}
+          alt="PK5 Mining Logo"
+          loading="lazy"
+          className="w-30 h-auto object-contain"
+        />
+      </div>
+    );
 
   // Not logged in
   if (!isAuthenticated) {
