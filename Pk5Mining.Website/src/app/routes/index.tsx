07147cdea8d6ef2db13e9fa1efbda999b40338c9
Babatunde Ojo <babatundeo@theluxev.com>
2026-04-10 17:36:44 +0100

    add agro

--- Pk5Mining.Website/src/app/routes/index.tsx
+++ Pk5Mining.Website/src/app/routes/index.tsx
@@ -2,14 +2,21 @@ import { useRoutes, Navigate } from "react-router-dom";
 import { clientRoutes } from "./client-routes";
 import { adminRoutes } from "./admin-routes";
 
-const allowAdminFeatures = import.meta.env.VITE_ALLOW_ADMIN_FEATURES === "true";
-
 export function AppRoutes() {
+  const hostname = window.location.hostname;
+  const isAgroPortal = hostname.includes("pk5agroallied");
+  
+  const allowAdminFeatures = import.meta.env.VITE_ALLOW_ADMIN_FEATURES === "true";
+
   const routes = [
-    ...clientRoutes,
+    ...(!isAgroPortal ? clientRoutes : []),
+
     ...(allowAdminFeatures ? adminRoutes : []),
-    
-    { path: "*", element: <Navigate to="/" replace /> } 
+
+    { 
+      path: "*", 
+      element: <Navigate to={isAgroPortal ? "/admin" : "/"} replace /> 
+    }
   ];
 
   return useRoutes(routes);
