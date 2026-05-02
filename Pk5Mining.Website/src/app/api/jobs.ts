3ff93d19c6cf19cda24ed57082a3d0e3ed6fcdb6
Babatunde Ojo <babatundeo@theluxev.com>
2026-05-03 00:06:36 +0100

    add api key flag

--- Pk5Mining.Website/src/app/api/jobs.ts
+++ Pk5Mining.Website/src/app/api/jobs.ts
@@ -74,7 +74,7 @@ export async function getJobsForDropdown() {
 
 export async function getJobById(id: string) {
   try {
-    const { data } = await http.get<ApiResponse<JobDto>>(`/Job/${id}`);
+    const { data } = await http.get<ApiResponse<JobDto>>(`/Job/${id}`, { requiresApiKey: true });
 
     if (data.responseStatus !== "SUCCESS") {
       throw new Error(
