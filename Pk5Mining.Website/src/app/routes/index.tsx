a9a0a64f5ff87b42f0750c6abdc8e498807c0d40
Babatunde Ojo <babatundeo@theluxev.com>
2026-05-12 20:07:31 +0100

    fix redirection

--- Pk5Mining.Website/src/app/routes/index.tsx
+++ Pk5Mining.Website/src/app/routes/index.tsx
@@ -2,21 +2,25 @@ import { useRoutes, Navigate } from "react-router-dom";
 import { clientRoutes } from "./client-routes";
 import { adminRoutes } from "./admin-routes";
 import { useTenant } from "@/tenants/useTenant";
+import { useAuth } from "../context/AuthContext";
 
- const allowAdminFeatures = import.meta.env.VITE_ALLOW_ADMIN_FEATURES === "true";
- 
- export function AppRoutes() {
+const allowAdminFeatures = import.meta.env.VITE_ALLOW_ADMIN_FEATURES === "true";
+
+export function AppRoutes() {
   const { isAgro } = useTenant();
+  const { user:authUser } = useAuth();
+
+  const path = authUser || isAgro ? "/admin" : "/"
 
   const routes = [
     ...(!isAgro ? clientRoutes : []),
 
     ...(allowAdminFeatures ? adminRoutes : []),
 
-    { 
-      path: "*", 
-      element: <Navigate to={isAgro ? "/admin" : "/"} replace /> 
-    }
+    {
+      path: "*",
+      element: <Navigate to={path} replace />,
+    },
   ];
 
   return useRoutes(routes);
