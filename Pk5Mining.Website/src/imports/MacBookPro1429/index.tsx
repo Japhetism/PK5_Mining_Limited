030fa03d0865709639a4a3b7fabd113b302178b6
Treasure Sam <treasures@theluxev.com>
2026-06-16 15:43:06 +0100

    login form in progress

---
+++ Pk5Mining.Website/src/imports/MacBookPro1429/index.tsx
@@ -0,0 +1,327 @@
+import svgPaths from "./svg-gadtsffos9";
+import imgImage98 from "./21c23672ce84dd03d9a6556cc4700d29aaae6e29.png";
+import imgImage15 from "./c36f9bd2b0feaff16979ab1ff556de16182a7aa1.png";
+import imgImage103 from "./0920571d191ee92fb66ed9adf50760ecb26368a5.png";
+
+function Group1() {
+  return (
+    <div className="absolute contents left-[2.11px] top-[17.13px]">
+      <p className="-translate-x-1/2 [text-box-edge:cap_alphabetic] [text-box-trim:trim-both] [word-break:break-word] absolute font-['Girassol:Regular',sans-serif] leading-[27.648px] left-[93.68px] not-italic text-[18px] text-center text-white top-[46.4px] tracking-[5px] whitespace-nowrap">MINING</p>
+      <p className="-translate-x-1/2 [word-break:break-word] absolute font-['Iceland:Regular',sans-serif] h-[20.88px] leading-[7.92px] left-[99.55px] not-italic overflow-hidden text-[60px] text-center text-ellipsis text-white top-[20.73px] tracking-[-2.4px] w-[97.44px] whitespace-nowrap">PK5</p>
+      <div className="absolute h-[20.4px] left-[2.11px] top-[17.13px] w-[51.6px]" data-name="image 15">
+        <div className="absolute inset-0 overflow-hidden pointer-events-none">
+          <img alt="" className="absolute h-[143.82%] left-0 max-w-none top-[-12.13%] w-full" src={imgImage15} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Group2() {
+  return (
+    <div className="absolute h-[61.083px] left-[-5.81px] top-[-3.26px] w-[60px]">
+      <div className="absolute inset-[-0.2%_-0.18%_0_-0.23%]">
+        <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 60.2414 61.2031">
+          <g id="Group 11">
+            <path d={svgPaths.p2c0e9960} id="Vector 7" stroke="var(--stroke-0, #E7DFCD)" strokeWidth="0.24" />
+            <path d={svgPaths.p344e8700} fill="var(--fill-0, #E7DFCD)" id="Rectangle 16" />
+          </g>
+        </svg>
+      </div>
+    </div>
+  );
+}
+
+function Pk5Logo() {
+  return (
+    <div className="-translate-x-1/2 -translate-y-1/2 absolute bg-[rgba(26,26,26,0)] h-[58.08px] left-[calc(50%+1.89px)] top-[calc(50%+1.2px)] w-[170.4px]" data-name="PK5 Logo">
+      <Group1 />
+      <Group2 />
+    </div>
+  );
+}
+
+function Group3() {
+  return (
+    <div className="absolute h-[67.547px] left-0 top-0 w-[65.76px]">
+      <div className="absolute inset-[-1.71%_-1.56%_0_-1.98%]">
+        <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 68.083 68.6989">
+          <g id="Group 12">
+            <path d={svgPaths.pdf49f00} id="Vector 5" stroke="url(#paint0_linear_1_18)" strokeWidth="2.304" />
+            <path d={svgPaths.p1fcf9a00} fill="var(--fill-0, #EAD09C)" id="Rectangle 15" />
+          </g>
+          <defs>
+            <linearGradient gradientUnits="userSpaceOnUse" id="paint0_linear_1_18" x1="1.30033" x2="68.6883" y1="23.4409" y2="37.03">
+              <stop stopColor="#C89B3C" />
+              <stop offset="1" stopColor="#EED7A7" />
+            </linearGradient>
+          </defs>
+        </svg>
+      </div>
+    </div>
+  );
+}
+
+function Pk5AgroAlliedLogo() {
+  return (
+    <div className="-translate-x-1/2 absolute h-[67px] left-[calc(50%-552px)] top-[47px] w-[184px]" data-name="PK5 Agro-allied logo">
+      <Pk5Logo />
+      <Group3 />
+    </div>
+  );
+}
+
+function FormPanel() {
+  return (
+    <div className="-translate-y-1/2 absolute drop-shadow-[0px_4px_2px_rgba(0,0,0,0.25)] h-[854px] opacity-65 overflow-clip right-0 top-1/2 w-[622px]" data-name="form panel">
+      <div className="-translate-x-1/2 absolute bottom-0 flex h-[854px] items-center justify-center left-[calc(50%+2px)] w-[626px]">
+        <div className="flex-none rotate-180">
+          <div className="h-[854px] relative w-[626px]" data-name="image 103">
+            <img alt="" className="absolute inset-0 max-w-none object-bottom pointer-events-none size-full" src={imgImage103} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Frame6() {
+  return (
+    <div className="-translate-x-1/2 -translate-y-1/2 absolute left-1/2 size-[79.2px] top-[calc(50%-238.89px)]">
+      <svg className="absolute block inset-0 size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 79.2 79.2">
+        <g id="Frame 189">
+          <rect fill="var(--fill-0, white)" fillOpacity="0.97" height="78.21" rx="39.105" width="78.21" x="0.495" y="0.495" />
+          <rect height="78.21" rx="39.105" stroke="var(--stroke-0, #C89B3C)" strokeWidth="0.99" width="78.21" x="0.495" y="0.495" />
+          <path d={svgPaths.p315eef00} fill="var(--fill-0, #C89B3C)" id="Vector" />
+        </g>
+      </svg>
+    </div>
+  );
+}
+
+function Group() {
+  return (
+    <div className="absolute contents inset-[39.37%_92.57%_36.38%_4.55%]" data-name="Group">
+      <div className="absolute inset-[39.37%_93.22%_50.24%_5.2%]" data-name="Vector">
+        <div className="absolute inset-[-10.11%_-7.94%]">
+          <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 7.58653 6.18213">
+            <path d={svgPaths.p1753f000} id="Vector" stroke="var(--stroke-0, #C89B3C)" strokeLinecap="round" strokeWidth="1.0395" />
+          </svg>
+        </div>
+      </div>
+      <div className="absolute inset-[49.76%_92.57%_36.38%_4.55%]" data-name="Vector">
+        <div className="absolute inset-[-7.58%_-4.33%]">
+          <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 13.0424 7.89634">
+            <path d={svgPaths.pd186900} id="Vector" stroke="var(--stroke-0, #C89B3C)" strokeLinecap="round" strokeWidth="1.0395" />
+          </svg>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Frame() {
+  return (
+    <div className="bg-white h-[49.5px] relative rounded-[10.395px] shrink-0 w-full">
+      <div className="overflow-clip relative rounded-[inherit] size-full">
+        <p className="[text-box-edge:cap_alphabetic] [text-box-trim:trim-both] [word-break:break-word] absolute font-['Inter:Light',sans-serif] font-light leading-[normal] left-[calc(50%-159.05px)] not-italic text-[14px] text-black top-[calc(50%-4.95px)] whitespace-nowrap">Enter your email</p>
+        <div className="absolute left-[11.9px] size-[25px] top-[13.49px]">
+          <svg className="absolute block inset-0 size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 25 25">
+            <circle cx="12.5" cy="12.5" fill="var(--fill-0, #D9D9D9)" fillOpacity="0.4" id="Ellipse 4" r="12.5" />
+          </svg>
+        </div>
+        <Group />
+      </div>
+      <div aria-hidden className="absolute border-[1.039px] border-[rgba(200,155,60,0.99)] border-solid inset-0 pointer-events-none rounded-[10.395px]" />
+    </div>
+  );
+}
+
+function CuidaArrowRightOutline() {
+  return (
+    <div className="h-[8.446px] relative shrink-0 w-[12.285px]" data-name="cuida:arrow-right-outline">
+      <svg className="absolute block inset-0 size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 12.285 8.44594">
+        <g clipPath="url(#clip0_1_10)" id="cuida:arrow-right-outline">
+          <path clipRule="evenodd" d={svgPaths.p21c40370} fill="var(--fill-0, black)" fillRule="evenodd" id="Vector" />
+          <path clipRule="evenodd" d={svgPaths.pa560200} fill="var(--fill-0, black)" fillRule="evenodd" id="Vector_2" />
+        </g>
+        <defs>
+          <clipPath id="clip0_1_10">
+            <rect fill="white" height="8.44594" width="12.285" />
+          </clipPath>
+        </defs>
+      </svg>
+    </div>
+  );
+}
+
+function Frame2() {
+  return (
+    <div className="-translate-x-1/2 -translate-y-1/2 absolute content-stretch flex gap-[9.356px] items-end left-[calc(50%-0.57px)] top-[calc(50%+0.2px)]">
+      <p className="[text-box-edge:cap_alphabetic] [text-box-trim:trim-both] [word-break:break-word] font-['Inter:Bold',sans-serif] font-bold leading-[normal] not-italic relative shrink-0 text-[14.553px] text-black whitespace-nowrap">Continue</p>
+      <CuidaArrowRightOutline />
+    </div>
+  );
+}
+
+function Frame7() {
+  return (
+    <div className="bg-[#c89b3c] h-[49.5px] overflow-clip relative rounded-[10.395px] shrink-0 w-full">
+      <Frame2 />
+    </div>
+  );
+}
+
+function Frame10() {
+  return (
+    <div className="content-stretch flex gap-[21px] items-center relative shrink-0 w-full">
+      <div className="h-0 relative shrink-0 w-[175px]">
+        <div className="absolute inset-[-1px_0_0_0]">
+          <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 175 1">
+            <line id="Line 4" stroke="var(--stroke-0, #C89B3C)" x2="175" y1="0.5" y2="0.5" />
+          </svg>
+        </div>
+      </div>
+      <p className="[text-box-edge:cap_alphabetic] [text-box-trim:trim-both] [word-break:break-word] font-['Inter:Regular',sans-serif] font-normal leading-[normal] not-italic relative shrink-0 text-[16px] text-white whitespace-nowrap">OR</p>
+      <div className="h-0 relative shrink-0 w-[175px]">
+        <div className="absolute inset-[-1px_0_0_0]">
+          <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 175 1">
+            <line id="Line 5" stroke="var(--stroke-0, #C89B3C)" x2="175" y1="0.5" y2="0.5" />
+          </svg>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Frame3() {
+  return (
+    <div className="-translate-x-1/2 -translate-y-1/2 absolute bg-black content-stretch flex items-end left-[calc(50%-0.55px)] top-[calc(50%-0.44px)]">
+      <p className="[text-box-edge:cap_alphabetic] [text-box-trim:trim-both] [word-break:break-word] font-['Inter:Bold',sans-serif] font-bold leading-[normal] not-italic relative shrink-0 text-[14.553px] text-white whitespace-nowrap">Sign in with SSO</p>
+    </div>
+  );
+}
+
+function Frame1() {
+  return (
+    <div className="bg-black h-[49.5px] relative rounded-[10.395px] shrink-0 w-full">
+      <div className="overflow-clip relative rounded-[inherit] size-full">
+        <Frame3 />
+      </div>
+      <div aria-hidden className="absolute border border-[rgba(255,255,255,0.8)] border-solid inset-0 pointer-events-none rounded-[10.395px]" />
+    </div>
+  );
+}
+
+function Frame11() {
+  return (
+    <div className="absolute content-stretch flex flex-col gap-[40px] items-start left-[103px] top-[345.51px] w-[415.9px]">
+      <Frame />
+      <Frame7 />
+      <Frame10 />
+      <Frame1 />
+    </div>
+  );
+}
+
+function Frame8() {
+  return (
+    <div className="absolute h-[850px] right-[-1px] top-[3px] w-[622px]">
+      <FormPanel />
+      <Frame6 />
+      <p className="[text-box-edge:cap_alphabetic] [text-box-trim:trim-both] [word-break:break-word] absolute font-['Segoe_UI:Semibold',sans-serif] leading-[normal] left-[calc(50%-99px)] not-italic text-[34px] text-white top-[calc(50%-167.61px)] whitespace-nowrap">Admin Login</p>
+      <p className="[text-box-edge:cap_alphabetic] [text-box-trim:trim-both] [word-break:break-word] absolute font-['Inter:Regular',sans-serif] font-normal leading-[normal] left-[calc(50%-207.9px)] not-italic text-[16px] text-white top-[calc(50%-104.25px)] whitespace-nowrap">Email Address</p>
+      <Frame11 />
+    </div>
+  );
+}
+
+function Frame9() {
+  return (
+    <div className="absolute bg-[rgba(0,0,0,0.46)] h-[117px] left-[47px] overflow-clip rounded-[18px] top-[656px] w-[607.5px]">
+      <div className="-translate-y-1/2 absolute flex h-[85.5px] items-center justify-center left-[202.5px] top-[calc(50%+0.75px)] w-0">
+        <div className="flex-none rotate-90">
+          <div className="h-0 relative w-[85.5px]">
+            <div className="absolute inset-[-1.5px_0_0_0]">
+              <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 85.5 1.5">
+                <line id="Line 2" stroke="var(--stroke-0, #C89B3C)" strokeWidth="1.5" x2="85.5" y1="0.75" y2="0.75" />
+              </svg>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="-translate-y-1/2 absolute flex h-[85.5px] items-center justify-center left-[405px] top-[calc(50%+0.75px)] w-0">
+        <div className="flex-none rotate-90">
+          <div className="h-0 relative w-[85.5px]">
+            <div className="absolute inset-[-1.5px_0_0_0]">
+              <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 85.5 1.5">
+                <line id="Line 2" stroke="var(--stroke-0, #C89B3C)" strokeWidth="1.5" x2="85.5" y1="0.75" y2="0.75" />
+              </svg>
+            </div>
+          </div>
+        </div>
+      </div>
+      <p className="[text-box-edge:cap_alphabetic] [text-box-trim:trim-both] [word-break:break-word] absolute font-['Inter:Bold',sans-serif] font-bold leading-[normal] left-[36px] not-italic text-[#c89b3c] text-[24px] top-[36px] whitespace-nowrap">3+</p>
+      <p className="[text-box-edge:cap_alphabetic] [text-box-trim:trim-both] [word-break:break-word] absolute font-['Inter:Bold',sans-serif] font-bold leading-[normal] left-[238.5px] not-italic text-[#c89b3c] text-[24px] top-[36px] whitespace-nowrap">2.3M</p>
+      <p className="[text-box-edge:cap_alphabetic] [text-box-trim:trim-both] [word-break:break-word] absolute font-['Inter:Bold',sans-serif] font-bold leading-[normal] left-[441px] not-italic text-[#c89b3c] text-[24px] top-[36px] whitespace-nowrap">99.9%</p>
+      <p className="[text-box-edge:cap_alphabetic] [text-box-trim:trim-both] [word-break:break-word] absolute font-['Rajdhani:Bold',sans-serif] leading-[normal] left-[calc(50%-267.75px)] not-italic text-[18px] text-white top-[calc(50%+10.5px)] whitespace-nowrap">ACTIVE SITES</p>
+      <p className="[text-box-edge:cap_alphabetic] [text-box-trim:trim-both] [word-break:break-word] absolute font-['Rajdhani:Bold',sans-serif] leading-[normal] left-[calc(50%-65.25px)] not-italic text-[18px] text-white top-[calc(50%+10.5px)] whitespace-nowrap">DAILY OUTPUT</p>
+      <p className="[text-box-edge:cap_alphabetic] [text-box-trim:trim-both] [word-break:break-word] absolute font-['Rajdhani:Bold',sans-serif] leading-[normal] left-[calc(50%+137.25px)] not-italic text-[18px] text-white top-[calc(50%+10.5px)] whitespace-nowrap">SYSTEM UPTIME</p>
+    </div>
+  );
+}
+
+function GameIconsMiningHelmet() {
+  return <div className="absolute left-[683px] size-[16px] top-[418px]" data-name="game-icons:mining-helmet" />;
+}
+
+function Frame5() {
+  return (
+    <div className="-translate-x-1/2 -translate-y-1/2 absolute bg-[rgba(0,0,0,0)] border border-[#c89b3c] border-solid h-[854px] left-1/2 overflow-clip top-1/2 w-[1384px]">
+      <div className="-translate-y-1/2 absolute h-[854px] left-[-1px] top-1/2 w-[1384px]" data-name="image 98">
+        <div className="absolute inset-0 overflow-hidden pointer-events-none">
+          <img alt="" className="absolute h-[101.87%] left-[-2.03%] max-w-none top-0 w-[102.04%]" src={imgImage98} />
+        </div>
+      </div>
+      <Pk5AgroAlliedLogo />
+      <p className="[text-box-edge:cap_alphabetic] [text-box-trim:trim-both] [word-break:break-word] absolute font-['Rajdhani:Bold',sans-serif] h-[132px] leading-[normal] left-[calc(50%-644px)] not-italic text-[64px] text-white top-[calc(50%-232px)] w-[652px]">
+        Powering the Future
+        <br aria-hidden />
+        of Mineral Development
+      </p>
+      <Frame8 />
+      <div className="absolute flex h-[2.195px] items-center justify-center left-[47px] top-[260px] w-[179.987px]">
+        <div className="flex-none rotate-[-0.7deg]">
+          <div className="h-0 relative w-[180px]">
+            <div className="absolute inset-[-1px_0_0_0]">
+              <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 180 1">
+                <line id="Line 1" stroke="var(--stroke-0, #C89B3C)" x2="180" y1="0.5" y2="0.5" />
+              </svg>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Frame9 />
+      <GameIconsMiningHelmet />
+      <p className="[text-box-edge:cap_alphabetic] [text-box-trim:trim-both] [word-break:break-word] absolute font-['Segoe_UI:Regular',sans-serif] h-[50px] leading-[normal] left-[47px] not-italic text-[22px] text-white top-[calc(50%-76px)] w-[530px]">Enterprise grade administrative control for global mining operations</p>
+    </div>
+  );
+}
+
+function Frame4() {
+  return (
+    <div className="-translate-x-1/2 -translate-y-1/2 absolute bg-white h-[982px] left-1/2 overflow-clip top-1/2 w-[1512px]">
+      <div className="absolute h-[982px] left-0 top-0 w-[1550px]" data-name="image 91" />
+      <Frame5 />
+    </div>
+  );
+}
+
+export default function MacBookPro() {
+  return (
+    <div className="bg-black relative size-full" data-name="MacBook Pro 14' - 29">
+      <div className="absolute h-[982px] left-0 top-0 w-[1512px]" data-name="image 89" />
+      <Frame4 />
+    </div>
+  );
+}
