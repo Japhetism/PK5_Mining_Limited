5487b6e3779b6358f53737e90877c34cfe4ee4a7
Babatunde Ojo <babatundeo@theluxev.com>
2026-06-17 17:10:53 +0100

    restriict to domain login

--- Pk5Mining.Website/src/app/pages/admin/login/index.tsx
+++ Pk5Mining.Website/src/app/pages/admin/login/index.tsx
@@ -120,7 +120,7 @@ export function Login() {
 
           {error && (
             <div className="text-sm text-red-500 bg-red-500/10 border border-red-500/40 rounded-md px-3 py-2">
-              {error}xx
+              {error}
             </div>
           )}
 
