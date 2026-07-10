8bebbc4b3e343e5fc0dc3c80b4031924a063d796
Babatunde Ojo <babatundeo@theluxev.com>
2026-07-10 08:10:52 +0100

    add loader

--- Pk5Mining.Website/src/app/pages/client/layout.tsx
+++ Pk5Mining.Website/src/app/pages/client/layout.tsx
@@ -3,6 +3,7 @@ import { Header } from "@/app/components/header";
 import { Footer } from "@/app/components/footer";
 import { ScrollToTop } from "@/app/components/scrollToTop";
 import { useLocation } from "@/app/hooks/useLocation";
+import Logo from "../../../assets/images/logo.png";
 
 const GLOBAL_URL = "https://pk5miningltd-test-v2.vercel.app";
 const US_HOST = "us-pk5miningltd-test-v2.vercel.app";
@@ -11,15 +12,21 @@ export function ClientLayout() {
   const { data, isLoading } = useLocation();
 
   if (isLoading) {
-    return null;
+    return (
+      <div>
+        <img
+          src={Logo}
+          alt="PK5 Mining Logo"
+          loading="lazy"
+          className="w-30 h-auto object-contain"
+        />
+      </div>
+    );
   }
 
   const host = window.location.hostname;
 
-  if (
-    host === US_HOST &&
-    data?.country_code !== "US"
-  ) {
+  if (host === US_HOST && data?.country_code !== "US") {
     window.location.replace(GLOBAL_URL);
     return null;
   }
