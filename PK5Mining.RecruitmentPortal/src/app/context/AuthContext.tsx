45e8f5321dc3cbdb18b83ef2b843949ff34243db
BABATUNDE OJO <babatunde.ojo@accessbankplc.com>
2026-07-29 08:52:17 +0100

    change default label & remove graph

--- PK5Mining.RecruitmentPortal/src/app/context/AuthContext.tsx
+++ PK5Mining.RecruitmentPortal/src/app/context/AuthContext.tsx
@@ -99,14 +99,6 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
           return;
         }
         const msToken = await authService.getToken();
-        const graphToken = await authService.getGraphToken();
-
-        if (graphToken) {
-          // for testing
-          const microsoftGraphResponse = await microsoftGraph(graphToken);
-          console.log("from auth context microsoft graph ", microsoftGraphResponse)
-        }
-        
         if (msToken) {
           setAuthToken(msToken);
           tokenStore.set(msToken);
